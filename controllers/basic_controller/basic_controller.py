9671bb63b56cbb8c1756ec285b16d95bf4fbe660
Piotr Patek <piotrpatek17@gmail.com>
2024-02-15 00:52:55 +0100

    #1 Added spherical feet for hexapod and simple wave gait

--- controllers/basic_controller/basic_controller.py
+++ controllers/basic_controller/basic_controller.py
@@ -60,10 +60,39 @@ servo53.setPosition(-math.pi/2)
 servo62.setPosition(math.pi/6)
 servo63.setPosition(-math.pi/2)
 
+class Leg:
+    def __init__(self, s1, s2, s3, offset, leg_name: str, sign):
+        self._s1 = s1
+        self._s2 = s2
+        self._s3 = s3
+        self._offset = offset
+        self._leg_name = leg_name
+        self._sign = sign
+
+    def __str__(self):
+        return self._leg_name
+
+    def move_leg(self):
+        print(1)
+        self._s2.setPosition(math.pi/5)
+        yield 2
+        print(2)
+        print(self._offset + (math.pi / 6)*self._sign)
+        self._s1.setPosition(self._offset + (math.pi / 6)*self._sign)
+        yield 2
+        print(3)
+        self._s2.setPosition(math.pi / 6 - 0.01)
+        yield 2
+        print(4)
+        self._s1.setPosition(self._offset)
+        yield 3
+        self._s2.setPosition(math.pi / 6)
+
+
 def test():
     #L2 Swing
     print("Fist phase")
-    servo22.setPosition(math.pi/7)
+    #servo22.setPosition(math.pi/7)
     yield 1
     print("Second phase")
     servo21.setPosition(math.pi/6)
@@ -74,26 +103,32 @@ def test():
 
 
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+
+
 t1 = time()
-x = test()
-i = 0
+leg1 = Leg(servo11, servo12, servo13, 0, "l1", -1)
+leg2 = Leg(servo21, servo22, servo23, 0, "l2", -1)
+leg3 = Leg(servo31, servo32, servo33, 0, "l3", -1)
+leg4 = Leg(servo41, servo42, servo43, math.pi, "l4", 1)
+leg5 = Leg(servo51, servo52, servo53, math.pi, "l5", 1)
+leg6 = Leg(servo61, servo62, servo63, math.pi, "l6", 1)
+
+leg_list = [leg1, leg4, leg2, leg5, leg3, leg6]
+
+leg = leg_list.pop()
+print(leg)
+g = leg.move_leg()
+
 while robot.step(timestep) != -1:
-    print(time() - t1)
-    if time() - t1 >= 5 and i < 3:
-        next(x)
+    if time() - t1 >= 0.1:
+        try:
+            next(g)
+        except:
+            leg_list.insert(0, leg)
+            leg = leg_list.pop()
+            print(leg)
+            g = leg.move_leg()
         t1 = time()
-        i += 1
-
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
     pass
 
 # Enter here exit cleanup code.
